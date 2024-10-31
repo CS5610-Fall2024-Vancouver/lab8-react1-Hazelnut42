70e1ecbf1ab04979e5614f4564d43e7f361b0da6
Hazel Chen <chen.zihe@northeastern.edu>
2024-10-30 17:14:13 -0700

    lab8

---
+++ src/components/Task.js
@@ -0,0 +1,16 @@
+// components/Task.js
+import { FaTrash } from 'react-icons/fa';
+
+function Task({ task, deleteTask }) {
+  return (
+    <li className="task-item">
+      <span className="task-text">{task.text}</span>
+      <FaTrash 
+        className="delete-icon"
+        onClick={() => deleteTask(task.id)}
+      />
+    </li>
+  );
+}
+
+export default Task;
---
+++ src/components/TasksList.js
@@ -0,0 +1,18 @@
+// components/TasksList.js
+import Task from './Task';
+
+function TasksList({ tasks, deleteTask }) {
+  return (
+    <>
+      {tasks.map((task) => (
+        <Task 
+          key={task.id} 
+          task={task} 
+          deleteTask={deleteTask} 
+        />
+      ))}
+    </>
+  );
+}
+
+export default TasksList;
